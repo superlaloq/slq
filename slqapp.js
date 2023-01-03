7d668b125dd9e2321c57926600f04d3003093365
Eduardo <eduardo.barria.cabrera@gmail.com>
2023-01-03 16:08:04 -0400

    trozo de codigo que me regresa error

--- slqapp.js
+++ slqapp.js
@@ -11,11 +11,14 @@ const slqapp = express();
 
 //slqapp.use(express.static('assets'));
 slqapp.use(express.static('public'));
-//slqapp.use('/static', express.static(__dirname + '/public'));
-
+slqapp.use('/static', express.static(__dirname + '/public'));
+// mi falta de experiencia no logra descifrar el desenlace final de este codigo 
 
 slqapp.get (`/`, (req, res) => {
 res.sendFile (path.join(__dirname, `index.html`));
+// Esta solicitud al metodo join, nos proporciona el 
+// cotejador de rutas. Crupie de las direcciones separadas
+// une trozos y de la solucion final del archivo, el direccionamiento.
    console.log(__dirname);
 });
 
