21ceaae859ed31ffb9b9603b26a0d48ffa2dd944
Eduardo <eduardo.barria.cabrera@gmail.com>
2023-01-05 07:29:18 -0400

    comentarios

--- slqapp.js
+++ slqapp.js
@@ -1,11 +1,17 @@
-const express = require("express");
-    // Declarando estas lineas implementamos un modulo que con su funsión 
-    // permite la creasión de un objeto http.
-const path = require (`path`);
-    // este camino nos lleba al archivo, un módulo dirigido al desarrollador
-    //     de los direccionamientos por ejemplo de los ficheros de la app.
+// slqapp
+// Servisio de base de datos para direccionamientos
+// requiere comprender basicamente html css js
+// servidor web local publico o privado 
+// eduardobarria dedicado a los estudiantes de fullStack JavaSript
+
+const express = require("express");// Declarando estas lineas implementamos un modulo que con su funsión 
+    // permite crear un objeto http Una especie de caja con carpetas dentro 
+    // o completamente vacías para almacenar programas de repositorios
+const path = require (`path`);// este camino nos lleba al archivo
+    // un módulo dirigido a direccionamientos por ejemplo los ficheros de la app
 const fs = require (`fs`);
-const http = require (`http`);
+const http = require (`http`);// este modulo permite el protocolo http fundamental para las web
+
 const slqapp = express();
 
 slqapp.use(express.static('assets'));
@@ -21,7 +27,7 @@ res.sendFile (path.join(__dirname, `index.html`));
    console.log(__dirname);
 });
 
-
+// el servidor entrega servicios y escucha pedidos de servicio, por el puerto 3000.
 slqapp.listen (3000, () => {
  console.log( `Servidor corriendo por el puerto`, 3000)
 });
