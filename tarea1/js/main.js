6595d14f16a31128a5d150ba721714779c0f2493
Eduardo <eduardo.barria.cabrera@gmail.com>
2023-01-08 13:18:54 -0400

    cambios

--- tarea1/js/main.js
+++ tarea1/js/main.js
@@ -19,3 +19,13 @@ if(variable.className=='rojo') {
 }
 
     }
+
+
+    lightbox.option({
+        'resizeDuration': 200,
+        'wrapAround': true,
+        'resizeDuration':100,
+        'showImageNumberLabel': true,
+        'albumLabel': 'Imagem %1 de %2',
+    });
+    
