86e6e8f6b5d10a60875aee9b701055fa6f0311ca
Eduardo <eduardo.barria.cabrera@gmail.com>
2023-01-11 10:09:37 -0400

    cambio archivos

--- slq2.0/slqapp.js
+++ slq2.0/slqapp.js
@@ -1,31 +1,35 @@
-// archivo sqapp.js
+   // slqapp
 
-const express = require('express'); // importamos express
-const http = require('http'); // importamos http
+   // requiere comprender basicamente html css js
+   // servidor web local publico o privado 
+   // eduardobarria dedicado a los estudiantes de fullStack JavaSript
 
-const port = 3000; // especificamos el puerto
-const path = __dirname; // especificamos el directorio
+const express = require("express");// Declarando estas lineas implementamos un modulo que con su funsión 
+    // permite crear un objeto http Una especie de caja con carpetas dentro 
+    // o completamente vacías para almacenar programas de repositorios
+const path = require (`path`);// este camino nos lleba al archivo
+    // un módulo dirigido a direccionamientos por ejemplo los ficheros de la app
+const fs = require (`fs`);
+const http = require (`http`);// este modulo permite el protocolo http fundamental para las web
 
-const app = express(); // inicializamos la app
+const slqapp = express();
 
-app.use(express.static(path)); // configuramos el servidor para servir archivos estáticos
+slqapp.use(express.static('assets'));
+slqapp.use(express.static('public'));
+slqapp.use('/static', express.static(__dirname + '/public'));
 
-// conectamos la base de datos
-const mysql = require('mysql');
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'username',
-  password: 'password',
-  database: 'db_name'
+    // mi falta de experiencia no logra descifrar el desenlace final de este codigo 
+
+slqapp.get (`/`, (req, res) => {
+res.sendFile (path.join(__dirname, `index.html`));
+
+    // Esta solicitud al metodo join, nos proporciona el 
+    // cotejador de rutas. Crupie de las direcciones separadas
+    // une trozos y de la solucion final del archivo, el direccionamiento.
+   console.log(__dirname);
 });
 
-db.connect((err) => {
-  if (err) {
-    console.log('Error al conectar con la base de datos: ' + err);
-  } else {
-    console.log('Conexión exitosa a la base de datos');
-  }
+    // el servidor entrega servicios y escucha pedidos de servicio, por el puerto 3000.
+slqapp.listen (3000, () => {
+ console.log( `Servidor corriendo por el puerto`, 3000);
 });
-
-// creamos el servidor
-const server = http
