8cfa433e772129430ec7118c567e602ad2c9bcd8
Eduardo <eduardo.barria.cabrera@gmail.com>
2023-01-16 11:49:25 -0400

    arreglo de archivos

--- bootstrap/cachurean2/pagina-con-fullpage-js-archivos_base/js/app.js
+++
@@ -1,3 +0,0 @@
-const seccionesPagina = new("#fullpage", {
-
-});
---
+++ laloq/experimentales/ruletadecolor.js
@@ -0,0 +1,11 @@
+let colors = ["Grey","red", "yellow", "Cyan", "blue", "red", "black", "Azure","Cyan", "green", "white", "Grey", "Azure",  "red", "black", "Yellow", "green", "red", "Azure", "white", "Cyan", "black", "Grey", "green", "red", "black", "white", "Azure", "green", "black", "red", "yellow", "Cyan", "red",  "Azure", "white", "red", "black", "Azure", "Yellow" ];
+let i = 0;
+
+setInterval(() => {
+  document.body.style.backgroundColor = colors[i];
+  i++;
+  if (i >= colors.length) i = 0;
+}, 3);
+
+alert("Bienvenido a SlQ, aquí encontraras Servicios y Productos Informáticos");
+      
