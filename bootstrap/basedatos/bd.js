e12a7c667a7c9159f27b5a73164b9adf7245952a
Eduardo <eduardo.barria.cabrera@gmail.com>
2023-01-18 22:42:59 -0400

    cambios de archivos

---
+++ bootstrap/basedatos/bd.js
@@ -0,0 +1,10 @@
+const productDB = {
+    "id": "1",
+    "codigo": 'A123',
+    "valor": "12.60",
+    "cantidad": "10",
+    "otros": {
+        "color": 'azul',
+        "tamaño": 'mediano'
+    }
+};
